0849a86f0ab8eac79bb54ff667757af099e36cd9
Aola-j <okunolaabe@gmail.com>
2025-09-10 12:18:43 -0500

    first init with zustand state

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+const App = () => {
+
+  return (
+    <div>
+      <AddRecipeForm/>
+      <RecipeList/>
+    </div>
+  )
+}
+
+export default App;
