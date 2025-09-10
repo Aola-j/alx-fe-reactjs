eaba088d8256bc38384d5743da700ffc61e7aabb
Aola-j <okunolaabe@gmail.com>
2025-09-10 14:33:36 -0500

    Router implementation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,12 @@
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Router } from 'react-router-dom'
 import RecipeDetails from './components/RecipeDetails'
 
 const App = () => {
 
   return (
-    <div>
+    <Router>
       <h1>My Recipes</h1>
 
       <AddRecipeForm/>
@@ -15,7 +15,7 @@ const App = () => {
       <Routes>
          <Route path="/recipes/:id" element={<RecipeDetailsWrapper />} />
       </Routes>
-    </div>
+    </Router>
   )
 }
 
