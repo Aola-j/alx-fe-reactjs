a39f066e017c0263698073a4d5746125133d2931
Aola-j <okunolaabe@gmail.com>
2025-09-10 14:11:49 -0500

    Included detailed recipe management

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,27 @@
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import { Routes, Route, Link } from 'react-router-dom'
+import RecipeDetails from './components/RecipeDetails'
 
 const App = () => {
 
   return (
     <div>
+      <h1>My Recipes</h1>
+
       <AddRecipeForm/>
       <RecipeList/>
+
+      <Routes>
+         <Route path="/recipes/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
     </div>
   )
 }
 
-export default App;
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams()
+  return <RecipeDetails recipeId={id} />
+}
+
+export default App
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,7 +1,26 @@
 import { create } from 'zustand'
 
-export const useRecipeStore = create(set => ({
+export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
-}));
+  
+  addRecipe: (newRecipe) => 
+    set((state)=> ({ 
+      recipes: [...state.recipes, newRecipe] })),
+     
+     
+   setRecipes: (recipes) => set({ recipes }),
+
+ // Delete by id
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  // Update by id
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+}))
