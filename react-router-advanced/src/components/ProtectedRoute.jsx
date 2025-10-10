b0829520b143dcaf8a83daae9709134817220d9c
Aola-j <okunolaabe@gmail.com>
2025-10-10 11:54:48 -0500

    advance routing with react router

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
+useAuth
 const isAuthenticated = () => {
   // Simulating an authentication check (replace with actual logic)
   return localStorage.getItem('authenticated') === 'true';
